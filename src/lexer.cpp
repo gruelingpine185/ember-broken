80b3065d2580c2996fb5df47486b9dcd6147b92a
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-01-03 00:41:03 -0500

    fixes segfault & memory leak when in destructor

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -93,7 +93,7 @@ namespace mbr {
         const char* start_ptr = this->_data.c_str() + start_pos.offset;
         const size_t lexeme_len = (this->_pos.offset - start_pos.offset);
         std::string_view lexeme(start_ptr, lexeme_len);
-        val.as_str = {start_ptr, lexeme_len};
+        val.as_str = this->_data.substr(start_pos.offset, lexeme_len);
         token tok = token(lexeme, val, start_pos);
         return tok;
     }
